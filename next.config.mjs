91aa3a7ce0df78e3c02124d606efc6ae0b4b5718
ismaelbcs <ismaelbcs@gmail.com>
2026-06-15 10:32:56 -0700

    Fix: Eliminado page.js raiz y configurada redireccion en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/es', // Redirige el dominio principal a la versión en español
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
