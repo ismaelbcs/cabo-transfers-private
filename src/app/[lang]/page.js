8a5e48579e6f5859d407add2a642b4a3529c7a18
ismaelbcs <ismaelbcs@gmail.com>
2026-06-15 14:51:15 -0700

    arreglango co

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -3,6 +3,7 @@
 
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { 
   CheckCircle, 
